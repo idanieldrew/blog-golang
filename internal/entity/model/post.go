2a08f92abb48b2a44b61af2f63a1f42dd1bf00f7
idanieldrew <danielgia35@gmail.com>
2022-06-22 10:20:22 +0430

    Post entity

---
+++ internal/entity/model/post.go
@@ -0,0 +1,16 @@
+package model
+
+import "time"
+
+type Post struct {
+	Id          uint      `json:"id"`
+	Title       string    `json:"title"`
+	Slug        string    `json:"slug"`
+	Details     string    `json:"details"`
+	Description string    `json:"description"`
+	BlueTick    bool      `json:"blue_tick"`
+	UserId      uint      `json:"user_id"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	DeletedAt   time.Time `json:"deleted_at"`
+}
